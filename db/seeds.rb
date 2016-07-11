65c30537e8cd4d0d19fd958a5aac7250d516a023
copley <qcopley@gmail.com>
2016-07-11 19:19:28 +1200

    First commit of training app

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Resume.create(name: 'Dan Bunker' , phone: '333-333-3333')
+Resume.create(name: 'Daadfn Bunkfadfaer' , phone: '36633-333-3333', city: 'egwergergergerg')
+
